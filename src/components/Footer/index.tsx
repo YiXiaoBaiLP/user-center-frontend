8519a2c69e1f685a82d9ef5cc522dda568cf383a
YiXiaoBai <yixiaobai.lp@qq.com>
2023-10-24 22:42:43 +0800

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,37 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+const Footer: React.FC = () => {
+  const defaultMessage = '小刘集团出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'Ant Design Pro',
+          title: 'Ant Design Pro',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: (
+            <>
+              <GithubOutlined />
+              小刘
+            </>
+          ),
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: false,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const SYSTEM_IMG = '';
+export const PLANET_LINK = 'http://www.baidu.com';
